510ebda20f9b16e3efcea63a39524ce848323c75
Manny <Manny@10.31.66.59>
2016-08-30 17:16:16 -0400

    Modified Lab3Exercise with Manuel's Changes

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,10 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("Manuel Baez");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(1200, 700);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
